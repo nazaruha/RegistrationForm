d814f6e3faa61bafef1cb5c0acef16a067f0793d
Nazar <fedunnazar20122001@gmail.com>
2022-11-14 10:14:59 +0200

    add js with upload photo logic

---
+++ js/app.js
@@ -0,0 +1,30 @@
+﻿//declearing html elements
+const imgDiv = document.querySelector('.profile-pic-div');
+const img = document.querySelector('#photo');
+const file = document.querySelector('#photo-file');
+const btnPhotoUpload = document.querySelector('#btn-photo-upload');
+
+//uploading photo
+file.addEventListener('change', function() {
+    //this refers to file
+    const choosedFile = this.files[0];
+
+    if (choosedFile) {
+        const reader = new FileReader(); // FileReader is a predefined function of JS
+
+        reader.addEventListener('load', function() {
+            img.setAttribute('src', reader.result);
+        });
+
+        reader.readAsDataURL(choosedFile);
+
+        //recovers image sizes to initial
+        if (img && img.style) {
+            img.style.height = '200px';
+            img.style.width = '200px';
+        }
+    }
+});
+
+
+
